f2c7091c528277d7e819d3575852916958250f91
qdelooze <>
2024-05-02 18:56:19 +0200

    work in progress

--- frontend/framework/components/game/game.js
+++ frontend/framework/components/game/game.js
@@ -2,95 +2,106 @@ import Component from "../component.js";
 import Map from "./map.js";
 import { CurrentPlayer, Player } from "./player.js";
 
-let lastUpdateTime = performance.now()
-let frameCount = 0
-let fps = document.getElementById("fps")
 
 export default class Game extends Component {
-    constructor(props, ws, username, players) {
+    constructor(props, ws, username, readyPlayers) {
         super("section", props);
         this.username = username;
         this.ws = ws;
-
         this.size = 19;
         this.atlas = this.ws.sendMessage({ type: "map" });
-
-        this.players = players
-        console.log("PLAYERS:", this.players);
+        this.readyPlayers = readyPlayers
+        this.playerMoveQueue = []
 
         this.ws.onMessage((message) => {
             if (message.type === "map") {
-                this.atlas = message.body
-                const map = new Map(this.atlas)
-                const positions = [
-                    { top: -608 +32, left: 32 },
-                    { top: -64 , left: 608 - 64 },
-                    { top: -608+32, left: 608 - 64 },
-                    { top: -64, left: 32 }
-                ]
-                this.players.forEach((player, index) => {
-
-                    const props = {
-                        id: player.children[0],
-                        className: "player-sprite",
-                        style: {
-                            top: positions[index].top,
-                            left: positions[index].left,
-                        }
-                    }
-
-                    if (player.children[0] !== this.username) {
-                        const newPlayer = new Player(props)
-                        map.addElement(newPlayer)
-                        this.players[index] = newPlayer
-                    } else {
-                        this.currentPlayer = new CurrentPlayer(
-                            props
-                            , this.ws,
-                            this.username)
-
-                        map.addElement(this.currentPlayer)
-                        this.players[index] = this.currentPlayer
-                    }
-                })
-
-                this.addElement(map)
-                this.update()
+                this.initMap(message)
             }
 
             if (message.type === "move") {
-                console.log("move received: ", message);
-                const player = this.players.find(player => player.props.id === message.sender)
-                player.move(message.position)
+                this.updatePlayers(message)
             }
         })
+
+
         this.FRAMERATE = 1000 / 60;
-        this.delta = 0;
         this.lastTime = 0
 
+        this.fps = document.getElementById("fps")
+        this.lastUpdateTime = Date.now()
+        this.frameCount = 0
 
         this.gameLoop()
     }
 
+    initMap(message){
+        this.atlas = message.body
+        const map = new Map(this.atlas)
+        const positions = [
+            { top: -608 + 32, left: 32 },
+            { top: -64, left: 608 - 64 },
+            { top: -608 + 32, left: 608 - 64 },
+            { top: -64, left: 32 }
+        ]
+        this.readyPlayers.forEach((player, index) => {
+
+            const props = {
+                id: player.children[0],
+                className: "player-sprite",
+                style: {
+                    top: positions[index].top,
+                    left: positions[index].left,
+                }
+            }
+
+            if (player.children[0] !== this.username) {
+                const newPlayer = new Player(props)
+                map.addElement(newPlayer)
+                this.readyPlayers[index] = newPlayer
+            } else {
+                this.currentPlayer = new CurrentPlayer(
+                    props
+                    , this.ws,
+                    this.username)
+
+                map.addElement(this.currentPlayer)
+                this.readyPlayers[index] = this.currentPlayer
+            }
+        })
+
+        this.addElement(map)
+        this.update()
+    }
+
+    updatePlayers(message) {
+        const player = this.readyPlayers.find(player => player.props.id === message.sender)
+        this.playerMoveQueue.push({ player, position: message.position })
+    }
+
     gameLoop(timestamp) {
-        let now = performance.now()
-        let delta = now - lastUpdateTime
-        if (delta >= 1000) {
-            fps.textContent = `${frameCount} FPS`
-            frameCount = 0;
-            lastUpdateTime = now;
-        }
+        this.fpsCounter()
         const deltaTime = timestamp - this.lastTime
         if (deltaTime >= this.FRAMERATE) {
             this.lastTime = timestamp
-            this.updateState()
-            frameCount++
+                this.updateState()
+                
         }
         requestAnimationFrame((timestamp) => this.gameLoop(timestamp))
     }
 
     updateState() {
+        this.playerMoveQueue.forEach((player) => {
+            player.player.move(player.position)
+        })
+        this.playerMoveQueue = []
     }
 
-
+    fpsCounter() {
+        if (Date.now() - this.lastUpdateTime >= 1000) {
+            this.fps.textContent = `FPS: ${this.frameCount}`
+            this.frameCount = 0
+            this.lastUpdateTime = Date.now()
+        }
+        this.frameCount++
+    }
 }
--- frontend/framework/components/game/player.js
+++ frontend/framework/components/game/player.js
@@ -1,6 +1,6 @@
-import Component from "../component.js";
+    import Component from "../component.js";
 
-export class Player extends Component {
+    export class Player extends Component {
     constructor(props, ws, username) {
         super("div", props);
         this.ws = ws;
@@ -11,62 +11,69 @@ export class Player extends Component {
     }
 
     draw() {
-        this.props.style = `transform: translate(${this.posX}px, ${this.posY}px);`
-        
+        this.props.style = `transform: translate(${this.posX}px, ${this.posY}px);`;
     }
 
     move(position) {
-        console.log("MOVING PLAYER");
         this.posX = position.x;
         this.posY = position.y;
         requestAnimationFrame(() => {
-            this.props.style = `transform: translate(${this.posX}px, ${this.posY}px);`
-            this.updateDOM();
-        })
+        this.props.style = `transform: translate(${this.posX}px, ${this.posY}px);`;
+        this.updateDOM();
+        });
+    }
     }
 
-
-}
-
-export class CurrentPlayer extends Player {
+    export class CurrentPlayer extends Player {
     constructor(props, ws, username) {
-        super(props, ws, username)
-        this.movementSize = 8
+        super(props, ws, username);
+        this.movementSize = 8;
+        window.addEventListener("keydown", (event) => {
+        let direction;
+        switch (event.key) {
+            case "ArrowUp":
+            case "z":
+                direction = "up";
+                this.posY -= this.movementSize;
 
-        window.addEventListener('keydown', (event) => {
-            let direction;
-            switch (event.key) {
-                case 'ArrowUp':
-                case 'z':
-                    direction = 'up';
-                    this.posY -= this.movementSize;
-                    break;
-                case 'ArrowDown':
-                case 's':
-                    direction = 'down';
-                    this.posY += this.movementSize;
-                    break;
-                case 'ArrowLeft':
-                case 'q':
-                    direction = 'left';
-                    this.posX -= this.movementSize
-                    break;
-                case 'ArrowRight':
-                case 'd':
-                    direction = 'right';
-                    this.posX += this.movementSize;
-                    break;
-            }
-            console.log(direction);
-            // this.movePlayer(direction);
-            this.ws.sendMessage({ type: "move", sender: this.username, position: { x: this.posX, y: this.posY } });
-        })
-    }
+                break;
+            case "ArrowDown":
+            case "s":
+            direction = "down";
+            this.posY += this.movementSize;
 
-    movePlayer(direction) {
+            break;
+            case "ArrowLeft":
+            case "q":
+            direction = "left";
+            this.posX -= this.movementSize;
+
+            break;
+            case "ArrowRight":
+            case "d":
+            direction = "right";
+            this.posX += this.movementSize;
+
+            break;
+        }
+
+        // console.log(this.posX, this.posY);
+
+        this.ws.sendMessage({
+            type: "move",
+            sender: this.username,
+            direction: direction,
+            position: { x: this.posX, y: this.posY },
+        });
+        // this.ws.sendMessage({ type: "move", sender: this.username, position: { x: this.posX, y: this.posY } });
+        });
     }
 
     dropBomb() {
-        this.ws.sendMessage({ type: "bomb", sender: this.username, position: { x: this.posX, y: this.posY } });
+        this.ws.sendMessage({
+        type: "bomb",
+        sender: this.username,
+        position: { x: this.posX, y: this.posY },
+        });
     }
-} 
+    }
