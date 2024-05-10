94b6fef2e65f6cc2f7b9b34bc3cf5a3005acd68f
GMZZ <legaynicolas76@gmail.com>
2024-05-10 09:26:54 +0200

    restart and endgame handled

--- frontend/framework/components/game/end-menu.js
+++ frontend/framework/components/game/end-menu.js
@@ -12,7 +12,6 @@ export default class EndMenu extends Component{
     initialize(){
         const title = new Component("h2", {id:"end-title"}, [`Game's over folks ! ${this.winner} wins !`])
         const winText = new Component("p", {id:"end-prompt"}, ["You can choose either to restart the game or to leave this game :"])
-        const warning = new Component("p", {id:"warning-end"},["WARNING ! IF YOU PRESS RESTART THIS WILL RESTART FOR ALL THE PLAYERS STILL IN THE LOBBY"])   
         const buttons = new Component("div",{id:"button-container"},[this.restartButton,this.leaveButton])
         this.addElement(title,winText,warning,buttons)
     }
